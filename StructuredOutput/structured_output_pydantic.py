13884606c227f2c92079a7978799bec7c69efd1d
Soham-KT <sohamkothariprof79@gmail.com>
2025-12-18 17:12:09 +0530

    local ollama integration

--- StructuredOutput/structured_output_pydantic.py
+++ StructuredOutput/structured_output_pydantic.py
@@ -1,32 +1,18 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_ollama import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage
 from pydantic import BaseModel
 
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="microsoft/Phi-3-mini-4k-instruct",
-    task="text-generation",
-    pipeline_kwargs={
-        "temperature": 0.5,
-        "max_new_tokens": 100
-    }
-)
-
-model = ChatHuggingFace(llm=llm)
+model = ChatOllama(model='llama2')
 
 class Schema(BaseModel):
     reply: str
     sentiment: str
 
-messages = [
-    SystemMessage(content='Output should be strictly JSON type'),
-    HumanMessage(content='how are you my guy')
-]
-
 structured_model = model.with_structured_output(Schema)
 
-res = structured_model.invoke(messages)
+res = structured_model.invoke('how are you my guy')
 
 print(res)
-# print(res["reply"])
-# print(res["sentiment"])
+print(res.reply)
+print(res.sentiment)
